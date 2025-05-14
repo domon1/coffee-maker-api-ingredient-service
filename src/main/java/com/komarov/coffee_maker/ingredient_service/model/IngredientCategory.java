a739969ba315e46df66478bd43ec8af2b05e7461
domon1 <dimoniy.dota2@mail.ru>
2025-05-15 00:05:53 +0300

    feat: Добавление моделей

---
+++ src/main/java/com/komarov/coffee_maker/ingredient_service/model/IngredientCategory.java
@@ -0,0 +1,26 @@
+package com.komarov.coffee_maker.ingredient_service.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+public class IngredientCategory implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Boolean isMultiplySelect;
+
+    @Column(nullable = false)
+    private String name;
+}
